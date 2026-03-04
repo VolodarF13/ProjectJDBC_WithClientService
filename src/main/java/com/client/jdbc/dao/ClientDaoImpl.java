effd343ead1b97b28b3ff3c69499bb17e653f3de
VolodarF13 <volodiabodnarchyk2000@gmail.com>
2026-03-04 09:29:25 +0200

    Refactoring...2

--- src/main/java/com/client/jdbc/dao/ClientDaoImpl.java
+++ src/main/java/com/client/jdbc/dao/ClientDaoImpl.java
@@ -2,7 +2,6 @@ package com.client.jdbc.dao;
 
 import com.client.jdbc.database.Database;
 import com.client.jdbc.entity.Client;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
